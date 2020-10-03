dd75395aaa4669c8ba948dc908815ba400a96f19
Tony Prado <34513822+tonnyprado@users.noreply.github.com>
2020-10-03 12:19:40 -0500

    Add files via upload

---
+++ ejer1.pl
@@ -0,0 +1,16 @@
+es_padre(terach, abraham).
+es_padre(terach, nachor).
+es_padre(terach, haran).
+es_padre(abraham, isaac).
+es_padre(haran, lot).
+es_padre(haran, milcah).
+es_padre(haran, yiscah).
+es_madre(sarah, isaac).
+es_hombre(terach).
+es_hombre(abraham).
+es_hombre(nachor).
+es_hombre(isaac).
+es_hombre(lot).
+es_mujer(sarah).
+es_mujer(milcah).
+es_mujer(yiscah).
---
+++ ejer2.pl
@@ -0,0 +1,40 @@
+es_padre(enrique7,arturo).
+es_padre(enrique7,margarita).
+es_padre(enrique7, enrique8).
+es_hermano(enrique8, arturo).
+es_hermano(enrique8, margarita).
+es_hermano(arturo,enrique8).
+es_hermano(arturo, margarita).
+es_hermana(margarita, arturo).
+es_hermana(margarita, enrique8).
+es_mujer(margarita).
+es_hombre(enrique7).
+es_hombre(enrique8).
+es_hombre(arturo).
+es_esposo(enrique8,catalina).
+es_esposo(enrique8,ana).
+es_esposo(enrique8,juana).
+es_esposa(catalina,enrique8).
+es_esposa(ana,enrique8).
+es_esposa(juana,enrique8).
+es_padre(enrique8,maria).
+es_padre(enrique8,isabel).
+es_padre(enrique8,eduardo).
+es_madre(catalina,maria).
+es_madre(ana,isabel).
+es_madre(juana,eduardo).
+es_abuelo(enrique7,maria).
+es_abuelo(enrique7,isabel.
+es_abuelo(enrique7,eduardo).
+es_tio(arturo,maria).
+es_tio(arturo,isabel).
+es_tio(arturo,eduardo).
+es_tia(margarita,maria).
+es_tia(margarita,isabel).
+es_tia(margarita,eduardo).
+es_hombre(eduardo).
+es_mujer(maria).
+es_mujer(isabel).
+es_mujer(catalina).
+es_mujer(ana).
+es_mujer(juana).
